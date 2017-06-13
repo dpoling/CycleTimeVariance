cc4cab759ce540a49c44c3a22e6b8eb5c0c83e77
Dan Poling <dan.poling@dh.com>
2017-06-13 08:08:26 -0700

    Work in process - phase 1

--- App.js
+++ App.js
@@ -8,26 +8,43 @@ Ext.define('CustomApp', {
         Ext.create('Rally.data.wsapi.Store', {
             model: 'userstory',
             autoLoad: true,
+            filters: [{
+                property: 'ScheduleState',
+                operator: '>',
+                value: 'Completed'
+            }],
+            limit: 'Infinity',
             listeners: {
                 load: this._onDataLoaded,
                 scope: this
             },
-            fetch: ['FormattedID', 'Name', 'ScheduleState', 'Tasks', 'Defects']
+            fetch: ['FormattedID', 'Name', 'ScheduleState', 'PlanEstimate', 'InProgressDate', 'AcceptedDate']
         });
     },
 
+    getFilters: function(){
+      return Rally.data.wsapi.Filter.fromQueryString("(ScheduleState > 'Completed')");  
+    },
+    
     _onDataLoaded: function(store, data) {
         var records = _.map(data, function(record) {
             //Perform custom actions with the data here
             //Calculations, etc.
+            var d1 = Rally.util.DateTime.fromIsoString(record.get('InProgressDate'));
+            var d2 = Rally.util.DateTime.fromIsoString(record.get('AcceptedDate'));
+            var days = Rally.util.DateTime.getDifference(d2,d1,'day');
+//            console.log('d1= ',d1);
+//            console.log('d2= ',d2);
+//            console.log('days= ',days);
             return Ext.apply({
-                TaskCount: record.get('Tasks').Count
+                daysCount: days
             }, record.getData());
         });
+        console.log('records= ',records);
 
         this.add({
             xtype: 'rallygrid',
-            showPagingToolbar: false,
+            showPagingToolbar: true,
             showRowActionsColumn: false,
             editable: false,
             store: Ext.create('Rally.data.custom.Store', {
@@ -38,27 +55,62 @@ Ext.define('CustomApp', {
                     xtype: 'templatecolumn',
                     text: 'ID',
                     dataIndex: 'FormattedID',
-                    width: 100,
+                    width: 70,
                     tpl: Ext.create('Rally.ui.renderer.template.FormattedIDTemplate')
                 },
                 {
                     text: 'Name',
                     dataIndex: 'Name',
-                    flex: 1
+                    flex: 2
+                },
+//                {
+//                    text: 'Schedule State',
+//                    dataIndex: 'ScheduleState',
+//                    width: 70
+//                },
+                {
+                    text: 'In-Progress Date',
+                    dataIndex: 'InProgressDate',
+                    flex: 1,
+                    renderer: function(value){
+                        if (value){
+                            var date = Rally.util.DateTime.fromIsoString(value);
+                            return Rally.util.DateTime.format(date, 'M d Y : h:i:s');
+                        }
+                        return '-- No InProgress Date --';
+                    }
                 },
                 {
-                    text: 'Schedule State',
-                    dataIndex: 'ScheduleState'
+                    text: 'Accepted Date',
+                    dataIndex: 'AcceptedDate',
+                    flex: 1,
+                    renderer: function(value){
+                        if (value){
+                            var date = Rally.util.DateTime.fromIsoString(value);
+                            return Rally.util.DateTime.format(date, 'M d Y : h:i:s');
+                        }
+                        return '-- No Accepted Date --';
+                    }
                 },
                 {
-                    text: '# of Tasks',
-                    dataIndex: 'TaskCount'
+                    text: 'Plan Est',
+                    dataIndex: 'PlanEstimate',
+                    width: 35
                 },
                 {
-                    text: '# of Defects',
-                    dataIndex: 'Defects',
-                    renderer: function(value) {
-                        return value.Count;
+                    text: 'Days',
+                    dataIndex: 'daysCount',
+                    width: 50,
+                    renderer: function(v,m,r){
+                        var p = r.get("PlanEstimate");
+                        console.log("V is ", v);
+                        console.log ("PlanEstimate is ", p);
+                        if (v > p){
+                            return "<font color='red' font-weight='bold' background-color='lightgray'>" + v + "</font>";
+                        }
+                        else {
+                            return "<font color='green' font-weight='bold' background-color='lightgray'>" + v + "</font>";
+                        }
                     }
                 }
             ]
